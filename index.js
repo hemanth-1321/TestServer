717706d0ab2a521eaf1808ad34693a07888dd738
hemanth-1321 <hemanth02135@gmail.com>
2024-10-15 13:34:16 +0530

    "req"

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("hello from express");
 });
-
+app.get("/check", (req, res) => {
+  res.send("hello from check");
+});
 app.listen(1000, () => {
   console.log("server up ===>1000");
 });
