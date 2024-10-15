c9a0403f89012c5f213ce2778a7ba65ce25b6b31
hemanth-1321 <hemanth02135@gmail.com>
2024-10-15 13:38:01 +0530

    "ok"

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.get("/", (req, res) => {
 app.get("/check", (req, res) => {
   res.send("hello from check");
 });
+app.get("/ok", (req, res) => {
+  res.send("hello from ok");
+});
 app.listen(1000, () => {
   console.log("server up ===>1000");
 });
